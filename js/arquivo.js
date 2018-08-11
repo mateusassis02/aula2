a9c7657432af608f9fbcba8f27f1478f0862755a
Mateus Assis <mateusassis_02@outlook.com>
2018-08-11 15:34:33 -0300

    Adicionando JavaScript no formulario

---
+++ js/arquivo.js
@@ -0,0 +1,14 @@
+function enviar(){
+
+  var nome = document.getElementById("text-nome");
+  //se o valor na variavel nome for diferente de vazio
+  if(nome.value != ""){
+      //mostrar mensagem com o Nome
+      alert("Obrigado " + nome.value + ", mas está área está em desenvovlimento")
+    } else{
+      //mostrar mensagem generica
+      alert("Obrigado, mas está área está em desenvolvimento")
+    }
+
+
+}
